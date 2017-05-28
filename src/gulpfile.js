a2f008a8e43444adc56f89ea8c1c19aeb90e4190
Jordan <jmoss636@gmail.com>
2017-05-28 22:58:09 +0100

    gulpfile reactJS change & chai downgrade

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,3 +1,14 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const pug = require('gulp-pug')
+const babel = require('gulp-babel')
+
+
+//transpile JSX to JS
+gulp.task('babel', () => {
+    return gulp.src('./publicDev/**/*.js').
+        pipe(babel({
+            plugins: ['transform-react-jsx']
+        })).
+        pipe(gulp.dest('./public/'))
+})
