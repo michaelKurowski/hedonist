46d8a8122f7d581a2d2d6d8077c6a8822df21332
Jordan <jmoss636@gmail.com>
2017-05-29 19:06:12 +0100

    Front-end start w/ Vue

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,12 +2,19 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const pug = require('gulp-pug')
 const babel = require('gulp-babel')
-const eslint = require('gulp-eslint');
 
+gulp.task('babel'), () => {
+    return gulp.src('./publicDev/js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./public'));
+}
 
 gulp.task('html', () => {
     return gulp.src('./publicDev/index.html').
     pipe(gulp.dest('./public/'))
 })
+gulp.task('watchHTML', () => gulp.watch('./publicDev/index.html', ['html']))
 
-gulp.task('default', () => gulp.start('html'));
+gulp.task('default', () => gulp.start('watchHTML'));
