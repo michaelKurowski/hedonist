e7587dea3a4a6fd4e29f72f5540c1cec648c4276
Michael Kurowski <Byamarro@gmail.com>
2017-05-29 21:06:15 +0200

    Authentication router scaffolding

---
+++ src/routers/authentication.js
@@ -0,0 +1,20 @@
+const User = require('../models/user')
+const express = require('express')
+const router = express.Router()
+const throwLog = require('ionic-error-logger')
+
+
+router.post('/signIn', (req, res) => {
+
+
+})
+
+router.post('/signUp', (req, res) => {
+	
+
+})
+
+router.post('/signOut', (req, res) => {
+
+
+})
