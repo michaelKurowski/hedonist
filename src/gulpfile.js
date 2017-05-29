a0d2e5dbcdf8e5950b3115af2d828443aec1004c
Michael Kurowski <Byamarro@gmail.com>
2017-05-29 21:22:57 +0200

    Gulp SASS script

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -15,6 +15,16 @@ gulp.task('html', () => {
     return gulp.src('./publicDev/index.html').
     pipe(gulp.dest('./public/'))
 })
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('../prod/css'))
+        .on('error', function (err) {console.log(err);this.emit('end')})
+})
+
+gulp.task('watchCSS', () => gulp.watch('./publicDev/**/*.scss', ['sass']))
+
 gulp.task('watchHTML', () => gulp.watch('./publicDev/index.html', ['html']))
 
 gulp.task('default', () => gulp.start('watchHTML'));
